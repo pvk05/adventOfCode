82efb48d138659f9451f21e06ed3d16007dc1861
pvk05 <philip.v.knudsen@gmail.com>
2023-12-12 23:43:03 +0100

    Add base code to tasks

---
+++ src/lib/advent/2023/js/07.js
@@ -0,0 +1,12 @@
+import * as txt from '$lib/advent/2023/inputs/07.txt?raw';
+
+export function task() {
+    let lines = txt.default.split('\n')
+
+    for (let line of lines) {
+        // skip empty lines
+        if(line == '') continue
+    }
+
+    return [0, 0]
+}

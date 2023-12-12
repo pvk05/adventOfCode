ad49ab9bed2d6adb3ea959fb15e437746c6ee6e2
pvk05 <philip.v.knudsen@gmail.com>
2023-12-12 23:38:19 +0100

    Add template for Advent of Code 2023 tasks

---
+++ src/lib/advent/2023/js/template.js
@@ -0,0 +1,14 @@
+import * as txt from '$lib/advent/2023/inputs/<day>.txt?raw';
+
+export function task() {
+    let lines = txt.default.split('\n')
+
+    for (let line of lines) {
+        // skip empty lines
+        if(line == '') continue
+
+        // code goes here
+    }
+
+    return [0, 0]
+}

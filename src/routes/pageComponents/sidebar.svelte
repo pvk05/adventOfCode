5c91e68fa9aa44aa7edb1e44c762d432ec3b5ced
pvk05 <philip.v.knudsen@gmail.com>
2023-12-14 22:38:14 +0100

    Add sidebar component for advent page

---
+++ src/routes/pageComponents/sidebar.svelte
@@ -0,0 +1,27 @@
+<script>
+    import { AppRail, AppRailTile, AppRailAnchor } from "@skeletonlabs/skeleton";
+    import { years } from "../advent/years.js";
+    import { page } from "$app/stores";
+
+    let currentTile = 0;
+</script>
+
+<div class=" h-full">
+    <AppRail>
+        {#each years as year, index}
+            <AppRailTile
+                bind:group={currentTile}
+                name="tile-{index}"
+                value={index}
+                title="tile-{index}"
+            >
+                <AppRailAnchor
+                    href={`/advent/${year.slug}`}
+                    selected={$page.url.pathname === `/advent/${year.slug}`}
+                >
+                    {year.slug}
+                </AppRailAnchor>
+            </AppRailTile>
+        {/each}
+    </AppRail>
+</div>

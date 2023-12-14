1cc427687571814edffc54078b5c0f4f28aec0f5
pvk05 <philip.v.knudsen@gmail.com>
2023-12-14 22:12:43 +0100

    Add layout components and update app.pcss

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,24 @@
 <script>
-  import "../app.pcss";
+    import "../app.pcss";
+    import { AppShell } from '@skeletonlabs/skeleton';
+    import Header from "./pageComponents/header.svelte";
+    import Sidebar from "./pageComponents/sidebar.svelte";
+    import { computePosition, autoUpdate, offset, shift, flip, arrow } from '@floating-ui/dom';
+    import { storePopup } from '@skeletonlabs/skeleton';
+    storePopup.set({ computePosition, autoUpdate, offset, shift, flip, arrow });
 </script>
 
-<slot />
+<AppShell>
+	<svelte:fragment slot="header"><Header/></svelte:fragment>
+	<svelte:fragment slot="sidebarLeft"><Sidebar /></svelte:fragment>
+	<!-- (sidebarRight) -->
+	<!-- (pageHeader) -->
+	<!-- Router Slot -->
+	<slot />
+	<!-- ---- / ---- -->
+	<!-- (pageFooter) -->
+	<!-- (footer) -->
+</AppShell>
 
 <svelte:head>
   <title>Advent of code</title>

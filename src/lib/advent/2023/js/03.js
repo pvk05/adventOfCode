e4e211b45f3c51cd70ccc2e421decaae6e2d8076
pvk05 <philip.v.knudsen@gmail.com>
2023-12-18 14:34:31 +0100

    Add console.log to iterate over lines

--- src/lib/advent/2023/js/03.js
+++ src/lib/advent/2023/js/03.js
@@ -3,9 +3,13 @@ import * as txt from '$lib/advent/2023/inputs/03.txt?raw';
 export function task() {
     let lines = txt.default.split('\n')
 
+    // iterate over lines
     for (let line of lines) {
         // skip empty lines
         if(line == '') continue
+
+        console.log(line)
+        
     }
 
     return [0, 0]
